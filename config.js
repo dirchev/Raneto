fa60fae4aeed507344aaeedddb744e162f42ed07
dirchev <dimitar.mirchev96@gmail.com>
2015-09-05 14:04:44 +0300

    updated config.js

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 var config = {
 
 	// Your site title (format: page_title - site_title)
-	site_title: 'Dimitar Mirchev Docs',
+	site_title: 'Dimitar Mirchev',
 
 	// The base URL of your site (can use %base_url% in Markdown files)
 	base_url: '',
 
 	// Used for the "Get in touch" page footer link
 	support_email: '',
-
-	// Footer copyright content
-	copyright: 'Copyright &copy; '+ new Date().getFullYear() +' - <a href="http://raneto.com">Powered by Raneto</a>',
 
 	// Excerpt length (used in search)
 	excerpt_length: 400,
